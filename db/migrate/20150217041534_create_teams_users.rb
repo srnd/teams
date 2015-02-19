80fe91ab7447081a46def2a2234d40c4be925273
TJ Horner <rowenrocks@gmail.com>
2015-02-18 18:13:20 -0800

    NEW DATABASE HLLOOLLO

--- db/migrate/20150217041534_create_teams_users.rb
+++ db/migrate/20150217041534_create_teams_users.rb
@@ -1,7 +1,6 @@
 class CreateTeamsUsers < ActiveRecord::Migration
   def change
-    drop_table :teams_users
-
+    # drop_table :teams_users
     create_join_table :teams, :users, column_options: {null: true} do |t|
       t.index [:team_id, :user_id]
       t.index [:user_id, :team_id]
