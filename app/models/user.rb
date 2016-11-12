6b7a1026e2161bd478795b773be28ce868c6c4bb
TJ Horner <me@tjhorner.com>
2016-11-11 20:48:59 -0800

    FIX BROKEN API

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,7 @@ class User < ActiveRecord::Base
 			:username => self.username,
 			:name => self.name,
 			:admin => self.admin,
-			:team => self.current_team
+			:team => self.current_team || false
 		}
 	end
 end
